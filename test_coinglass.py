b90d39b25bbf37957c68eba6042764eff4b3bd1c
aziodiw-alt <aziodiw@icloud.com>
2026-07-22 20:37:45 +0200

    Add files via upload

---
+++ test_coinglass.py
@@ -0,0 +1,26 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv("COINGLASS_API_KEY")
+
+url = "https://open-api-v4.coinglass.com/api/futures/global-long-short-account-ratio/history"
+
+headers = {
+print("API_KEY:", API_KEY)
+print("TYPE:", type(API_KEY))
+}
+
+params = {
+    "exchange": "Binance",
+    "symbol": "BTCUSDT",
+    "interval": "h4",
+    "limit": 5
+}
+
+response = requests.get(url, headers=headers, params=params, timeout=20)
+
+print("STATUS:", response.status_code)
+print(response.text[:1000])
